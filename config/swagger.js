94c6c506dc8a4d85fc3870f4cbfef64a980f522b
Sophia <sophiafrancine.tan@neu.edu.ph>
2025-12-05 04:16:58 +0800

    swagger

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   servers: [
     {
       url: 'https://studentinformationapi.onrender.com/api-docs/#/',
-      description: 'Local Server',
+      description: 'Swagger Server',
     },
   ],
   components: {
